cf9e45d8eb9465c2a0fd69eaec74981ec7f964c9
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-22 20:29:56 +0300

    final commit

--- Data Analysis/Sakila Python Data Analysis/sakila_utili.py
+++ Data Analysis/Sakila Python Data Analysis/sakila_utili.py
@@ -2,3 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 
+def create_conn():
+    engine = create_engine('mysql+pymysql://root:Esoteric90@!@localhost/sakila') 
+    return engine.connect()
+     
