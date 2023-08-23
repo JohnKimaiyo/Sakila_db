b9a9b90941ee1f7bf87357964b88b00cfdf69058
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-23 13:55:47 +0300

    final commit

--- Data Analysis/Sakila Python Data Analysis/sakila_utili.py
+++ Data Analysis/Sakila Python Data Analysis/sakila_utili.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 
 def create_conn():
-    engine = create_engine('mysql+pymysql://root:Esoteric90@!@localhost/sakila') 
+    engine = create_engine('mysql+pymysql://root:Esoteric90@!@localhost:3377/sakila') 
     return engine.connect()
+
+def run_query(query);
+conn = create_conn()
+df = pd.read_sql_query(query,conn)
+conn.close()
+return df
      
