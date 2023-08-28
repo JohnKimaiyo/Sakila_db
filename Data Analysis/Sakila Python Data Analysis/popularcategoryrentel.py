b532ad43ce84f6b81da08a9486707188c2765c58
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-28 18:11:38 +0300

    final commit

---
+++ Data Analysis/Sakila Python Data Analysis/popularcategoryrentel.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot  as  plt       
+from sakila_utils import run_query
+
+
+def popular_film_categories();
+query = '''
+
+SELECT fc.category_id,c.name as 
+
+'''
+df = run_query(query)
+return df
---
+++ src/components/sql/max.sql
@@ -0,0 +1,4 @@
+--SELECT max(rental_rate)--
+
+
+SELECT max(rental_rate) FROM sakila.film;
